db4ae76d3602ec60681587fa06e3c037c73647a5
Martina <bartoli.martina87@gmail.com>
2022-03-16 20:20:35 +0100

    Added the units conversion

--- script.js
+++ script.js
@@ -1,87 +1,64 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[date.getDay()];
     let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
     let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    let dayIndex = date.getDay();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let day = days[dayIndex];
-  
-    return `${day} ${hours}:${minutes}`;
-  }
-  
-  function displayWeatherCondition(response) {
-    document.querySelector("#city").innerHTML = response.data.name;
-    document.querySelector("#temperature").innerHTML = Math.round(
-      response.data.main.temp
-    );
-  
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML = Math.round(
-      response.data.wind.speed
-    );
-    document.querySelector("#description").innerHTML =
-      response.data.weather[0].main;
-  }
-  
-  function searchCity(city) {
-    let apiKey = "4ea6767d0abc3c35728fd35103a03a69";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeatherCondition);
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    let city = document.querySelector("#city-input").value;
-    searchCity(city);
-  }
-  
-  function searchLocation(position) {
-    let apiKey = "4ea6767d0abc3c35728fd35103a03a69";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  
-    axios.get(apiUrl).then(displayWeatherCondition);
-  }
-  
-  function getCurrentLocation(event) {
-    event.preventDefault();
-    navigator.geolocation.getCurrentPosition(searchLocation);
-  }
-  
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = 66;
-  }
-  
-  function convertToCelsius(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = 19;
-  }
-  
-  let dateElement = document.querySelector("#date");
-  let currentTime = new Date();
-  dateElement.innerHTML = formatDate(currentTime);
-  
-  let searchForm = document.querySelector("#search-form");
-  searchForm.addEventListener("submit", handleSubmit);
-  
-  let currentLocationButton = document.querySelector("#current-location-button");
-  currentLocationButton.addEventListener("click", getCurrentLocation);
-  
-  searchCity("New York");
-  
+    return '${day}, ${hours}: ${minutes} pm';
+}
+
+function displayTemperature(response) {
+  celsiusTemperature = response.data.main.temp;
+  document.querySelector(".temp").innerHTML = Math.round(celsiusTemperature);
+  document.querySelector(".city").innerHTML = response.data.name;
+  document.querySelector(".description").innerHTML = response.data.weather[0].description;
+  document.querySelector(".humidity").innerHTML = response.data.main.humidity;
+  document.querySelector(".wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector(".date").innerHTML = formatDate(response.data.dt *1000);
+  let iconElement = document.querySelector(".icon");
+  iconElement.setAttribute("src", 'https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png');
+}
+
+function search(city) {
+  let apiKey = "4ea6767d0abc3c35728fd35103a03a69";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function changeCity(event) {
+  event.preventDefault();
+ let searchBarElement = document.querySelector("#city-input");
+ search(searchBarElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", changeCity);
+
+function showFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemp = (celsiusTemperature*9)/5+32;
+  let temperatureElement = document.querySelector(".temp");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector(".fharenheit");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+function showCelsius(event) {
+  event.preventDefault();
+ let temperatureElement = document.querySelector(".temp");
+ temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusLink = document.querySelector(".celsius-active");
+celsiusLink.addEventListener("click",showCelsius);
+
+search("Rome");
+
+
+
+
+
+    
